cc3dc5ad23e2c93acfe19862228d2a5d4059f535
Hans Jakob Emmel <hans.emmel@gmail.com>
2015-04-06 07:54:03 -0300

    colocando código IBGE e lógica para adicionar o mesmo na tabela de cidades e estados

--- br_populate.rb
+++ br_populate.rb
@@ -5,19 +5,19 @@ require 'json'
 module BRPopulate
   def self.states
     http = Net::HTTP.new('raw.githubusercontent.com', 443); http.use_ssl = true
-    JSON.parse http.get('/zigotto/br_populate/master/states.json').body
+    JSON.parse http.get('/hjemmel/br_populate/master/states.json').body
   end
 
   def self.capital?(city, state)
-    city == state["capital"]
+    city['name'] == state["capital"]
   end
 
   def self.populate
     ActiveRecord::Base.transaction do
       states.each do |state|
-        state_obj = State.find_or_create_by(acronym: state["acronym"], name: state["name"])
+        state_obj = State.find_or_create_by(acronym: state["acronym"], name: state["name"], code: state['code'])
 
-        state["cities"].each { |city| City.find_or_create_by(name: city, state: state_obj, capital: capital?(city, state)) }
+        state["cities"].each { |city| City.find_or_create_by(name: city['name'], state: state_obj, code: city['code'], capital: capital?(city, state)) }
       end
     end
   end
